46ff2286b2fa7e0407a48767eec7f87c4e97a662
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-08-04 14:40:51 +0300

    update catch_a_bird

--- Makefile
+++ Makefile
@@ -1,5 +1,20 @@
 lint:
-	poetry run flake8 catch_a_bird.py
+	poetry run flake8 catch_a_bird
 
 install:
 	poetry install
+
+build: # сборка пакета
+	poetry build
+
+publish: # отладка публикации
+	poetry publish --dry-run
+
+package-install: # установка пакета в систему
+	python3 -m pip install --user dist/*.whl
+
+package-reinstall: # переустановка пакета в систему после обновления
+	pip install --user --force-reinstall dist/*.whl
+
+catch-bird: # запуск программы
+	poetry run python -m catch_a_bird
--- catch_a_bird.py
+++ catch_a_bird.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import numpy as np
 
 
@@ -15,21 +16,21 @@ class CatAndBird:
 
 def main():
     print('Сможет ли кошка поймать птичку?')
-    print('Зависит от нескольких факторов:')
+    print('Зависит от нескольких факторов:\n')
     print('Наличие кошки')
     print('Наличие птички')
     print('Здоровье кошки')
     print('Болезнь птички')
     print('Птичка в кошкиной доступности')
     print('У птички нет укрытия')
-    print('У птички нет защитников')
+    print('У птички нет защитников\n')
 
     assessment_of_factors = input('Оцените каждый фактор(0 - нет, 1 - да), '
                                   'запишите одной строкой через пробел: ')
     assessment_of_factors = list(map(int, assessment_of_factors.split()))
     weight = [5, 5, 3, 3, 3, 1, 1]
     cat_and_bird = CatAndBird(weight)
-    bias = 7
+    bias = 10
     result_sum = cat_and_bird.to_summarize(assessment_of_factors) - bias
     the_result_of_the_capture = cat_and_bird.activate(result_sum)
 
